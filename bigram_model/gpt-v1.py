f59b0363c21eca8e6695d2101eca81ed984a0558
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-06-29 12:56:50 +0530

    gpt module created

---
+++ bigram_model/gpt-v1.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import os
+import re
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+device
+max_iters = 100000
+learning_rate = 3e-4
+block_size = 8
+batch_size = 4
+eval_iters = 10000
+n_emb = 384
+n_layers = 4
+n_head = 4
+
+
+class GPTLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embeding_table = nn.Embedding(vocab_size, n_emb)
+        self.position_embedding_table = nn.Embedding(vocab_size, n_emb)
+        self.blocks = nn.Sequential(*[Block(n_emb, n_head=n_head) for _ in range(n_layers)])
+
+        self.ln_f = nn.LayerNorm(n_emb)
+        self.lm_head = nn.Linear(n_emb, vocab_size)
+
+        self.apply(self._init_weights)
+
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, index, targets=None):
+        logits = self.token_embeding_table(index)
+
+        tok_emb = self.token_embeding_table(index)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+        x = tok_emb + pos_emb
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lm_head(x)
+
+        if targets is None:
+            loss = None
+        
+        else:
+            B,T,C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+    
+
+    def generate(self, index, max_new_token):
+        for _ in range(max_new_token):
+            logits, loss = self.forward(index)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            index_next = torch.multinomial(probs, num_samples=1)
+            index = torch.cat((index, index_next))
+        return index
